f1631df38edeb613295c7e3f9fe51eda98cc3423
malakosss <35021438+malakosss@users.noreply.github.com>
2021-11-17 09:35:41 -0600

    Update external.py

--- external.py
+++ external.py
@@ -1,3 +1,3 @@
 import sys
-sys.path.append("/home/vlad/marcos_pack/marcos_client")
-sys.path.append("/home/vlad/marcos_pack/flocra-pulseq")
+sys.path.append("/home/hailab/mgh_marcos/marcos_client")
+sys.path.append("/home/hailab/mgh_marcos/flocra-pulseq")
